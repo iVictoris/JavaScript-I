de788db6356b48bc8eedbba7bf33008b4a35d384
Victor Tran <victor.n.tran@outlook.com>
2019-10-12 18:54:03 -0700

    complete intro objects challenge

--- assignments/objects.js
+++ assignments/objects.js
@@ -18,27 +18,74 @@ const example = {
 }
 
 // Write your intern objects here:
+// 1, mmelloy0@psu.edu, Mitzi, F
+const mutzi = {
+  id: 1,
+  name: 'Mitzi',
+  email: 'mmelloy0@psu.edu',
+  gender: 'F'
+}
 
+// 2, kdiben1@tinypic.com, Kennan, M
+const kennan = {
+  id: 2,
+  name: 'Kennan',
+  email:'kdiben1@tinypic.com',
+  gender: 'M'
+}
+
+// 3, kmummery2@wikimedia.org, Keven, M
+const keven = {
+  id: 3,
+  name: 'Keven',
+  email: 'kmummery2@wikimedia.org',
+  gender: 'M'
+}
+// 4, gmartinson3@illinois.edu, Gannie, M
+const gannie = {
+  id: 4,
+  name: 'Gannie',
+  email: 'gmartinson3@illinois.edu',
+  gender: 'M'
+}
+// 5, adaine5@samsung.com, Antonietta, F
+const antonietta = {
+  id: 5,
+  name: 'Antonietta',
+  email: 'adaine5@samsung.com',
+  gender: 'F'
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
+console.log(mutzi.name);
 
 // Kennan's ID
+console.log(kennan.id);
 
 // Keven's email
+console.log(keven.email);
 
 // Gannie's name
+console.log(gannie.name);
 
 // Antonietta's Gender
+console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
+kennan.speak = function() {
+  return `Hello, my name is ${this.name}!`
+}
 // console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+antonietta.multiplyNums = function(a, b) {
+  return a * b
+}
+// console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
